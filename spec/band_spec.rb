5f3c6692278fc01cd266088dd06efa084442f52e
jasandine <jasandine@gmail.com>
2015-02-04 11:58:04 -0800

    Adds a method that will capitalize the first word of a venue name, spec passes

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe(Band) do 
   it { should have_and_belong_to_many(:venues) }
 
-  it("capitilizes the first word of each band entered") do 
+  it("capitalizes the first word of each band entered") do 
     band = Band.create({:name => "gza"})
     expect(band.name).to(eq("Gza"))
   end
